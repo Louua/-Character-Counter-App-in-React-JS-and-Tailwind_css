60b9e79a916f0e82587d473df71e6d6eddd6d4db
Loua Ben Afia <loua12benafia@gmail.com>
2025-04-10 14:16:58 +0100

    set up

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      darkBlack: "#12131A",
+      lightBlack: "#21222C",
+      DarkGray: "#2A2B37",
+      lightGray: "#404254",
+
+      Sliver: "#E4E4EF",
+      lightSliver: "#F2F2F7",
+      WhiteColor: "#FFFFFF",
+
+      purpleColor: "#D3A0FA",
+      darkPurple: "#C27CF8",
+
+      yellowColor: "#FFF900",
+      OrangeColor: "#FE8159",
+      darkOrange: "#DA3701",
+    },
     extend: {},
   },
   plugins: [],
